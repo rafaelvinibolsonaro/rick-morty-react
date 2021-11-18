a3d973cbdaaf6de2c40f844b24c736077fb6b2af
rafaelvinibolsonaro <rv29394@gmail.com>
2021-11-18 18:14:15 -0300

    Chamando o componente principal

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HeaderResponsive from './Components/HeaderResponsive';
+import MainSection from './Components/MainSection';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderResponsive />
+      <MainSection />
+    </div>
+  );
+}
+
+export default App;
